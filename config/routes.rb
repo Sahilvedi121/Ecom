d90dae911ca8666815958b49877ef0d5164a31ec
sahil <shivamr357@gmail.com>
2024-12-05 19:15:11 +0530

    You

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  # get "products/index"
+  # get "profiles/index"
+  # get "profiles/show"
+  # get "profiles/new"
+  # # get "products/index"
   # get "products/new"
   # get "products/edit"
    devise_for :users
 
    resources :products, only: [:index, :create, :show, :edit, :update, :new, :destroy]
+   resources :profiles, only: [:index, :show, :new, :create, :edit, :update]
+
   root to: "products#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_05_065259) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_05_123452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,6 +40,8 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_05_065259) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -69,5 +71,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_05_065259) do
   add_foreign_key "addresses", "users"
   add_foreign_key "delivery_details", "products"
   add_foreign_key "delivery_details", "users"
+  add_foreign_key "products", "users"
   add_foreign_key "profiles", "users"
 end
